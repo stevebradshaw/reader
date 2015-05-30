66222692199558ed0629516eb9f9380fd95ad7cc
Steve Bradshaw <sjb1970@gmail.com>
2015-05-30 05:14:53 +0100

    First commit

---
+++ app.js
@@ -0,0 +1,41 @@
+var appinfo = { port: 3000 }
+  , express = require('express')
+  , stylus = require('stylus')
+  , nib = require('nib')
+  , logger = require('morgan') ;
+
+var app = express() ;
+
+var http = require('http').Server(app) ;
+
+app.locals.basedir = __dirname ;
+
+function compile(str, path) {
+  return stylus(str)
+    .set('filename', path)
+    .use(nib());
+}
+
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jade')
+
+app.use(logger("combined")) ;
+
+app.use(stylus.middleware(
+  { src: __dirname + '/public'
+  , compile: compile
+  }
+))
+
+app.use(express.static(__dirname + '/public')) ;
+
+app.get('/', function (req,res) {
+  res.render('index',
+       { title : 'Feed Reader' }
+     )
+  }) ;
+
+
+http.listen(appinfo.port, function(){
+  console.log('listening on *:3000');
+}) ;
---
+++ public/js/feed.js
@@ -0,0 +1,9 @@
+
+
+function setupButtons() {
+
+}
+
+$(document).ready(function() {
+	setupButtons() ;
+}) ;
